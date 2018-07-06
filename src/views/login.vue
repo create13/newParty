06a87ecbf66d8a91d7833fde131074ab9d509daf
create13 <1974931363@qq.com>
2018-07-06 16:27:39 +0800

    newParty

---
+++ src/views/login.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div class="out-wrap">
+      <Row>
+        <i-col span="12">
+          <i-input v-model="value" size="large" placeholder="large size"></i-input>
+        </i-col>
+        <i-col span="12"></i-col>
+      </Row>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data () {
+    return {
+      value: ''
+    };
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted () {},
+
+  methods: {}
+};
+
+</script>
+<style scoped>
+  .out-wrap {
+    width: 100%;
+  }
+</style>
