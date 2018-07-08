84ab3328b9a5187a31eab07313a231a0eb59a921
create13 <1974931363@qq.com>
2018-07-08 22:26:52 +0800

    home change

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,19 +1,19 @@
 <template>
   <div>
     <div class="out-wrap">
-      <Row>
+      <!-- <Row>
         <i-col span="12">
           <i-input v-model="value" size="large" placeholder="large size"></i-input>
         </i-col>
         <i-col span="12"></i-col>
-      </Row>
+      </Row> -->
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: '',
+  name: 'login',
   data () {
     return {
       value: ''
@@ -33,5 +33,9 @@ export default {
 <style scoped>
   .out-wrap {
     width: 100%;
+    height: 100%;
+    background-image: url(../assets/image/img-bg.jpg);
+    background-repeat:  no-repeat;
+    background-size: 100% 100%;
   }
 </style>
